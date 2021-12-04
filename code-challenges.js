546698ca8a06c06786b6d9c3d21709b779b75bec
DeonFranklin <deondeefranklin@gmail.com>
2021-12-04 15:33:45 -0800

    About 70% done. Will re-visit later.

--- code-challenges.js
+++ code-challenges.js
@@ -9,31 +9,58 @@
 
 // Create the code and test each of the variables provided. Expected output: "42 is below boiling point", "350 is above boiling point", "212 is at boiling point"
 
-var temp = 42
-// var temp = 350
-// var temp = 212
+var temp = 58
+// let temp2 = 350
+// let temp3
+// var temp3 = 212
+    if(temp >= 42 && temp <= 211){
+        console.log(`${temp} is below boiling point.`);
+    }
+var temp2 = 350
+
+    if(temp2 >= 213 && temp === 350){
+        console.log(`${temp} is above boiling point.`);
+    }
+var temp3 = 212
+    if(temp >= 211 && temp <= 213){
+         console.log(`${temp} is at boiling point.`);
+    }
+
+// // // --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
+
+// var myNumbers1 = [3, 7, 0, 36, -9]
+// var myNumbers2 = [8, -7, 42, 9, 13]
+
+// let myNumbers = (myNumbers1.concat(myNumbers2))
+// console.log(myNumbers.length)
 
 
-// --------------------2) Create the code that will combine the two arrays and return the length using the variables provided below. Expected output: 10
+// // --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "1202 ohcE"
 
-var myNumbers1 = [3, 7, 0, 36, -9]
-var myNumbers2 = [8, -7, 42, 9, 13]
-
-
-// --------------------3) Create the code that will reverse the letters of a string using the test variable provided below. Expected output: "1202 ohcE"
-
-var currentCohort = "Echo 2021"
+// var currentCohort = 'Echo 2021"]
 
 
-// --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
+
+
+// // --------------------4) Create a statement that will evaluate whether each number in an array is even or odd. Use the test variable provided below. Expected output: "odd" "even" "odd" "even" "odd" "even"
 
 var myArray = [13, 34, 5, 10, 27, 42]
 
+    for(let i = myArray; myArray % 2 === 0; myArray);{
+    console.log('Even', myArray);
+    if(myArray % 2 !== 0);
+    (console.log('Odd', myArray));
+    }
+// // --------------------5) Create a statement that evaluates two numbers and subtracts the smaller number from the larger number using the two sets of test variables provided below. Expected output: 42, 3
 
-// --------------------5) Create a statement that evaluates two numbers and subtracts the smaller number from the larger number using the two sets of test variables provided below. Expected output: 42, 3
+// var number1 = 58
+// var number2 = 100
 
-var number1 = 58
-var number2 = 100
+// console.log(number2 - number1)
 
 // var number1 = 27
-// var number2 = 24
+// var number2 = 24  
+
+// console.log(number1 - number2)
+
+;
--- interview-questions.md
+++ interview-questions.md
@@ -7,25 +7,25 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is git? What is the difference between git and Github?
 
-  Your answer:
+  Your answer: Git is a system used for version control to manage the save states of a project. Github is an application that hosts repositories and maintains branches of a project.
 
-  Researched answer:
+  Researched answer: Upon my research, I discovered Git is a version control system. Github is the cloud based Git repository manager and host.
 
 
 
 2. What types of evaluations will return a Boolean value?
 
-  Your answer:
+  Your answer: The evaluations that will return Boolean values are those that require the output of true or false and use a logical operator.
 
-  Researched answer:
+  Researched answer: Boolean evaluations require the return of values that are truthy or falsy. Boolean is the logical representation of true or false.
 
 
 
 3. What is an index? What is the difference between index and value?
 
-  Your answer:
+  Your answer: An index is the position of a character with in a series of characters in a string or an array. A value is data input into one of the primitive data types used to create evaluations.
 
-  Researched answer:
+  Researched answer: An index is the position of a value in within an array.
 
 
 
@@ -39,15 +39,15 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 5. Give a brief description of proper pair programming techniques. What are the roles of each person?
 
-  Your answer:
+  Your answer: Pair programming is the method in which two people work together to write code. The two roles are driver and navigator. The driver is actively coding, while the navigator directs the driver in creating the code.
 
-  Researched answer:
+  Researched answer: Pair programming is when two developers, the driver and the navigator, work together on the same workstation to complete a project. The role of the driver is to be on the keyboard inputting code. The navigator's role is to instruct the driver, and oversee the code being input 
 
 
 
 6. What is something we did in class this week you found helpful?  
 
-  Your answer:
+  Your answer: Something we did in class that was helpful this week was pair programming. Having a co-pilot definitely helped at times when I felt stuck or needed another perspective to aid in troubleshooting and correcting errors. 
 
 
 
